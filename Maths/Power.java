1e8abb4b0598f1557f4b8580738bf8bb9991945d
HITESH-235 <codonstream.72@gmail.com>
2025-10-18 00:10:08 +0530

    18 Oct 2025

---
+++ Maths/Power.java
@@ -0,0 +1,36 @@
+// -100.0 < base < 100.0
+// -231 <= exp <= 231-1
+// exp is an integer.
+// Either base is not zero or exp > 0.
+// -10^4 <= base^exp <= 10^4
+// LEETCODE link: https://leetcode.com/problems/powx-n/description/
+
+package Maths;
+public class Power {
+    public static void main(String[] args) {
+        System.out.println(power(2,-31));
+    }
+public static double power(double base, int exp) {
+    if (exp == 0) return 1;
+    if (base == 1) return base;
+
+    long exp2 = exp;
+    if (exp2<0) {
+        base = 1.0/base;
+        exp2 = -exp2;
+    }
+
+    double res = 1;
+    while (exp2>0) {
+        if (exp2%2 != 0) {
+            res *= base;
+            exp2 -= 1;
+        }
+        else {
+            base *= base;
+            exp2 >>= 1;
+        }
+    }
+    return res;
+}
+} 
